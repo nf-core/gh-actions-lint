0aab7f99db7d6d6f60a28f908bd4da0e0d2ffa50
maxibor <maxime.borry@gmail.com>
2020-07-15 16:00:27 +0200

    move install to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,14 @@
 set -ex
 set -o pipefail
 
+install_nextflow(){
+    conda install -c bioconda nextflow=${NEXTFLOW_VERSION}
+}
+
+install_nf_core_tools(){
+    pip install nf-core==${NFCORE_VERSION}
+}
+
 nf-core-lint() {
     nf-core lint ${GITHUB_WORKSPACE}
 }
@@ -15,6 +23,8 @@ yaml-lint(){
     yamllint $(find ${GITHUB_WORKSPACE} -type f -name "*.yml")
 }
 
+install_nextflow
+install_nf_core_tools
 nf-core-lint
 markdown-lint
 yaml-lint
